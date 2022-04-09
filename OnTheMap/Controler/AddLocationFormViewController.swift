e63a7a3c1ac6f32d21d683d508ad221c38e6cbca
segnonna <hounsouh@gmail.com>
2022-04-09 22:52:24 +0000

    fix review

--- OnTheMap/Controler/AddLocationFormViewController.swift
+++ OnTheMap/Controler/AddLocationFormViewController.swift
@@ -21,30 +21,31 @@ class AddLocationFormViewController : UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        getLocation(from: addressTextField.text ?? "") { location in
-            self.latitude = location?.latitude ?? 0.0
-            self.longitude = location?.longitude ?? 0.0
-        }    }
+
+    }
     
     @IBAction func closeScreen(_ sender: Any) {
         navigationController?.dismiss(animated: true)
     }
     
     @IBAction func findLocation(_ sender: Any) {
-        if (!(addressTextField?.text?.isEmpty ?? false)  && !(mediaUrl?.text?.isEmpty ?? false)) {
-            print(OnMapClient.Sessions.sessionId)
-            let storyboard = UIStoryboard (name: "Main", bundle: nil)
-            let resultVC = storyboard.instantiateViewController(withIdentifier: "MyCurrentLocationMapViewController") as! MyCurrentLocationMapViewController
-            resultVC.address = addressTextField?.text ?? "USA"
-            resultVC.mediaUrl = mediaUrl?.text ?? "http://www.google.com"
-            resultVC.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-            self.navigationController?.pushViewController(resultVC, animated: true)
-        } else {
-            let alertVC = UIAlertController(title: "Error", message: "You must fill out all the fileds", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.navigationController?.topViewController?.present(alertVC, animated: true)
+        getLocation(from: addressTextField.text ?? "") { location in
+        
+            if (!(self.addressTextField?.text?.isEmpty ?? false)  && !(self.mediaUrl?.text?.isEmpty ?? false)) {
+                print(OnMapClient.Sessions.sessionId)
+                let storyboard = UIStoryboard (name: "Main", bundle: nil)
+                let resultVC = storyboard.instantiateViewController(withIdentifier: "MyCurrentLocationMapViewController") as! MyCurrentLocationMapViewController
+                resultVC.address = self.addressTextField?.text ?? "USA"
+                resultVC.mediaUrl = self.mediaUrl?.text ?? "http://www.google.com"
+                resultVC.coordinate = CLLocationCoordinate2D(latitude: location?.latitude ?? 0.0 , longitude: location?.longitude ?? 0.0)
+                self.navigationController?.pushViewController(resultVC, animated: true)
+            } else {
+                let alertVC = UIAlertController(title: "Error", message: "You must fill out all the fileds", preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.navigationController?.topViewController?.present(alertVC, animated: true)
+            }
         }
+        
     }
     
     private func getLocation(from address: String, completion: @escaping (_ location: CLLocationCoordinate2D?)-> Void) {
